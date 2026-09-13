0036718fa0db93734b643f3e5b2ef0ecbf3fab06
DaniloSsoares <danilo.ssoares201@gmail.com>
2026-09-13 00:44:57 -0300

    fix: correção

--- teste.php
+++ teste.php
@@ -20,11 +20,11 @@ function lerLivro(Book $livro, string $descricao)
     echo "2. Página virada! Página atual: " . $livro->getPage() . "\n\n";
 }
 
-// 1. Testando Livro Físico (implementação nativa de Book)
+
 $livroFisico = new PaperBook();
 lerLivro($livroFisico, "Livro de Papel (PaperBook)");
 
-// 2. Testando Kindle (incompatível) adaptado pelo EBookAdapter
+
 $kindle = new Kindle();
 $kindleAdaptado = new EBookAdapter($kindle);
 lerLivro($kindleAdaptado, "Kindle usando EBookAdapter");
